47f4bced09c01d4c63a1cc2c8ddfac991f1ded06
camila capdevila <capdecami@gmail.com>
2021-02-15 19:18:15 -0300

    modificado

--- carrito.js
+++ carrito.js
@@ -17,7 +17,7 @@ class UI {
        {
 
         const productlist=document.getElementById('product.list');
-        const element =  document.createElement('div');
+        const element =   new document.createElement('div');
         element.innerHTML= `
                           <div class="card text-center mb-4">
 
@@ -26,41 +26,68 @@ class UI {
                          <strong>Product name:</strong>: ${product.name}
                          <strong>Product price:</strong>: ${product.price}
                          <strong>Product year:</strong>: ${product.year}
-                         
+                         <a href="#" class="btn btn-danger" name="delete">delete</a>
                          </div>
 
                           </div>
                             `;
               productlist.appendChild(element);
+              this.resetform();
+
          
        }
 
-    deleteproduct () {
+       resetform (){
+           document.getElementById('product-form').reset();
 
+       }
+
+
+    deleteproduct (element) {
+      if (element.name==='delete')
+      {
+          console.log(element.parentElement.parentElement.parentElement.remove())
+      }
     }
 
-    showmessage(){
+    showmessage(message,cssClass){
+     const div=document.createElement('div');
+     div.className=`alert alert-$(cssClass)`;
+     div.appendChild(document.createTextNode(message));
+     //mostrar en el dom
+   const container= document.querySelector('.container');
 
     }
 }
 
+sessionStorage.setItem("products",JSON.stringify(product));
+
+let parseproducts=JSON.parse(sessionStorage.getItem(product));
 
 // DOM events
 
 document.getElementById('product-form').addEventListener('submit',function(e)
-                                                                { 
+                                                                {    
+                                                                    e.preventDefault();
                                                                      const name=document.getElementById('name').value;
                                                                      const price=document.getElementById('price').value;
                                                                      const year=document.getElementById('year').value;
 
                                                                     
                                                                      
-                                                                     const product=new product(name,price,year);
+                                                                     const product_=new product(name,price,year);
                                                                      const ui=new UI();
-                                                                     ui.addproduct(product);
+                                                                     ui.addproduct(product_);
+                                                                    
+                                                                     
+                                                                 });
 
-                                                                     e.preventDefault();
-                                                                 })
+                                                                 document.getElementById('prodct-list').addEventListener('click',function(e)
+                                                                                                                                  {
+                                                                                                                                     const ui=new UI ();
+                                                                                                                                     ui.deleteproduct(e.target);
+
+                                                                                                                                  }            );
 
 
 
